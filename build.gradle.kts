e4c9893cd7ec7d106da4eb67b49411120581dc7d
shinyay <shinya.com@gmail.com>
2019-10-18 07:45:02 +0900

    Add spring-cloud-starter-kubernetes:1.0.3.RELEASE

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.cloud:spring-cloud-starter-kubernetes:1.0.3.RELEASE")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
